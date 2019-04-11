a8dfbc1a92883cae17eb2fb025d94f39b7a9236b
Lexel Yu <Lexel_Yu@asus.com>
2019-04-11 09:19:15 +0800

    Update comment

--- Hello.c
+++ Hello.c
@@ -31,7 +31,7 @@ void Output_string_length(void)
 	printf("Output string length: %d\n", (unsigned int)strlen(input_str));
 }
 
-// Function description
+// Function description v2
 int main(void)
 {
 	printf("Hello World\n");
